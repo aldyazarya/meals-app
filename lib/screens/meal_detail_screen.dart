edb2549465d7ae843685d69254e174032f31696c
aldyazarya <aldy1612@gmail.com>
2019-12-11 23:55:20 +0700

    create meal detail page and navigating to the meal detail page

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 class MealDetailScreen extends StatelessWidget {
+  static const routeName = './meal-detail';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('The Meal!'),
-    );
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final mealId = routeArgs['id'];
+    final mealTitle = routeArgs['title'];
+
+    return Scaffold(
+      appBar: AppBar(title: Text('$mealTitle'),),
+      body: Center(
+      child: Text('The Meal - $mealId!'),
+    ) ,
+    ); 
   }
 }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/meal_detail_screen.dart';
 
 import '../models/meal.dart';
+import '../screens/meal_detail_screen.dart';
 
 class MealItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
   final int duration;
@@ -42,6 +45,7 @@ class MealItem extends StatelessWidget {
   }
 
   MealItem({
+    @required this.id,
     @required this.title,
     @required this.imageUrl,
     @required this.duration,
@@ -49,12 +53,20 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
   });
 
-  void selectMeal() {}
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      MealDetailScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
