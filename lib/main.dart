7a702855be5a5f5d818ab56dfbfc2f50288f7769
aldyazarya <aldy1612@gmail.com>
2019-12-11 17:23:43 +0700

    create initialRoute and define aroute names using static const

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,11 @@ class MyApp extends StatelessWidget {
                 fontFamily: 'RobotoCondensed',
                 fontWeight: FontWeight.bold,
               ))),
-      home: CategoriesScreen(),
+      // home: CategoriesScreen(),
+      initialRoute: '/', //default is '/'
       routes: {
-        '/category-meals': (ctx) => CategoryMealsScreen()
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen()
       },
     );
   }
